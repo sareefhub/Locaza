29885a29d631f26b07b6b7f88c68002b0efe7cbe
ซารีฟ มะสาแม็ง <sareef.masamaeng@gmail.com>
2025-10-02 01:57:12 +0700

    chore(env): ปรับปรุง .env และ .gitignore พร้อมแก้ไข config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     BASE_URL: str = "http://localhost:8000"
 
     class Config:
-        env_file = ".env.docker"
+        env_file = ".env"
 
 
 settings = Settings()
